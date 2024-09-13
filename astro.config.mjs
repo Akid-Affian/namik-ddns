bf398191ae6f92336b233bbcab1afb92f7335e57
AkidAffian <denniskidman88@gmail.com>
2024-09-13 17:39:01 +0800

    Refactor Astro config and Dockerfile for improved server configuration and deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
@@ -6,7 +6,22 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  server: {
+    host: import.meta.env.HOST || '0.0.0.0',
+    port: import.meta.env.PORT || 4321
+  },
+  experimental: {
+    env: {
+      schema: {
+        BASE_DOMAIN: envField.string({ context: 'server', access: 'public', optional: true }),
+        NAMESERVERS: envField.string({ context: 'server', access: 'public', optional: true }),
+        PORT: envField.number({ context: 'server', access: 'public', default: 4321 }),
+        NODE_ENV: envField.string({ context: 'server', access: 'public', default: 'development' }),
+        HOST: envField.string({ context: 'server', access: 'public', default: '0.0.0.0' }),
+      },
+    },
+  },
 });
