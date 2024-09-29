6982115403d1052ffd9f62fd4c36daff1bfb194b
AkidAffian <denniskidman88@gmail.com>
2024-09-29 15:23:46 +0800

    Refactor astro.config.mjs to remove security checkOrigin option

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  security: {
-    checkOrigin: true
-  },
   adapter: bun({
     mode: "standalone",
   }),
